52175f541bf16c3c83ea8c29258aa1fea763fd9a
Thulasi k <thulasik112001@gmail.com>
2024-06-20 16:17:44 +0530

    initial commit

---
+++ src/app/models/Product.interface.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  // Other product properties as needed (discountPercentage, rating, etc.)
+}
